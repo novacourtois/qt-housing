fae9de24f649617873b08c34f1dfd620d20d1ad9
Jess Scheuring <jscheuring@gmail.com>
2016-10-22 19:52:58 -0500

    material ui form

--- public/app/scripts/router.js
+++ public/app/scripts/router.js
@@ -3,15 +3,29 @@ import {Router, Route, browserHistory} from 'react-router';
 import { Provider } from 'react-redux';
 import Login from './containers/login';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ThemeManager from 'material-ui/styles/getMuiTheme';
 
-const Root = ({ store }) => (
-  <Provider store={store}>
-		<MuiThemeProvider>
-	    <Router history={browserHistory}>
-	      <Route path="/" component={Login} />
-	    </Router>
-		</MuiThemeProvider>
-  </Provider>
-);
+const Root = React.createClass({
+	childContextTypes: {
+		 muiTheme: React.PropTypes.object
+ },
+
+ getChildContext() {
+		 return {
+				 muiTheme: ThemeManager()
+		 };
+ },
+	render() {
+		return (
+		  <Provider store={this.props.store}>
+				<MuiThemeProvider>
+			    <Router history={browserHistory}>
+			      <Route path="/" component={Login} />
+			    </Router>
+				</MuiThemeProvider>
+		  </Provider>
+		)
+	}
+});
 
 export default Root;
