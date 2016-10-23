2307d967812d217e467311648c1b5d6bf5538bb6
Jess Scheuring <jscheuring@gmail.com>
2016-10-22 22:14:15 -0500

    scaffold basic routes

--- public/app/scripts/router.js
+++ public/app/scripts/router.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Router, Route, browserHistory} from 'react-router';
 import { Provider } from 'react-redux';
 import Login from './containers/login';
+import Register from './containers/Register';
+import Setup from './containers/Setup';
+import Profile from './containers/Profile';
+import Matches from './containers/Matches';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ThemeManager from 'material-ui/styles/getMuiTheme';
 
@@ -21,6 +25,10 @@ const Root = React.createClass({
 				<MuiThemeProvider>
 			    <Router history={browserHistory}>
 			      <Route path="/" component={Login} />
+			      <Route path="/register" component={Register} />
+			      <Route path="/setup" component={Setup} />
+			      <Route path="/profile" component={Profile} />
+			      <Route path="/matches" component={Matches} />
 			    </Router>
 				</MuiThemeProvider>
 		  </Provider>
