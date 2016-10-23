c58964b993507831cdcf1f851dc4c1e3015ff42d
Jess Scheuring <jscheuring@gmail.com>
2016-10-23 13:45:20 -0500

    makes request to wave

--- public/app/scripts/components/cardDetail.js
+++ public/app/scripts/components/cardDetail.js
@@ -2,17 +2,22 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 export default React.createClass({
-	viewMatch() {
-		this.props.select(this.props.user);
+	wave() {
+		this.props.wave(this.props.token, this.props.me, this.props.user._id);
+	},
+	goBack() {
+		this.props.resetMatches();
 	},
 	render() {
 		return (
-			<Card onClick={this.viewMatch}>
+			<Card>
+				<a href="#" onClick={this.goBack}>Back</a>
 		    <CardTitle title={this.props.user.username} subtitle={this.props.user.pronouns}/>
 		    <CardText>
 		      {this.props.user.headline}
 		    </CardText>
 		    <CardActions>
+					<button onClick={this.wave}>wave</button>
 		    </CardActions>
 		  </Card>
 		)
